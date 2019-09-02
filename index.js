82c74a896d92673307de00fb26e3f7ec76e80e95
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-09-02 18:31:29 +0530

    Templating, Scss Middlewares Added

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express')
 const chalk = require('chalk')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const sassMiddleware = require('node-sass-middleware')
+
+const { logger } = require('./middlewares/logger')
 
 app = express()
 
+// Middlewares
+app.use(logger)
 app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// Template Settings
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// Sass Middleware
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public', 'css'),
+  prefix: '/css',
+  outputStyle: "compressed",
+  debug: true,
+  response: false
+}))
 
 app.get('/', (req, res) => {
-  res.send("Welcome")
+  let context = {
+    title: 'GitHub Supreme'
+  }
+  res.render('index', context)
 })
 
 const PORT = process.env.PORT || 5000
---
+++ routes/api/users.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send("Get Users")
+})
+
+router.get('/:id', (req, res) => {
+  res.send("Get a User")
+})
+
+router.post('/', (req, res) => {
+  res.send("Create a User")
+})
+
+router.put('/:id', (req, res) => {
+  res.send("Update a User")
+})
+
+router.delete('/:id', (req, res) => {
+  res.send("Delete a User")
+})
+
+module.exports = router 
