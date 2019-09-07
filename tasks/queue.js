ff8bf48268ca69da06ee1aff130da6c96ddc954e
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-09-07 08:59:29 +0530

    NodeResque Queue Added

--- tasks/queue.js
+++ tasks/queue.js
@@ -1,48 +1,54 @@
-const NR = require('node-resque')
+const NodeResque = require('node-resque')
 const chalk = require('chalk')
+const mongoose = require('mongoose')
 
-async function processQueue() {
-  let connectionDetails = {
+const config = require('../config')
+
+const fetchRepositories = require('./fetchRepositories')
+const fetchCollaborators = require('./fetchCollaborators')
+const fetchCollaboratorDetails = require('./fetchCollaboratorDetails')
+
+async function processQueue(user) {
+  const connectionDetails = {
     pkg: 'ioredis',
-    host: '127.0.0.1',
-    password: null,
-    port: 6379,
+    host: config.REDIS_HOST,
+    password: config.REDIS_PASSWORD,
+    port: config.REDIS_PORT,
     database: 0
   }
 
-  const jobs = {
-    'subtract': {
-      perform: (a, b) => {
-        let answer = a - b
-        return answer
-      }
-    },
-    fetchData: function (arg_data, callback) {
-      console.log(chalk.yellow.inverse(arg_data))
-      let data = {
-        name: "Jitendra Nirnejak",
-        login: "nirnejak"
-      }
-      console.log(chalk.red(arg_data))
-      callback(err, data)
-    }
-  }
+  let jobsToComplete = 0
 
-  const queue = new NR.Queue({ connection: connectionDetails }, jobs)
+  const jobs = {
+    'fetchRepositories': {
+      plugins: ['JobLock'],
+      pluginOptions: { JobLock: {} },
+      perform: fetchRepositories(user)
+    },
+    'fetchCollaborators': { perform: fetchCollaborators(user) },
+    'fetchCollaboratorDetails': { perform: fetchCollaboratorDetails(user) }
+  }
+  const queue = new NodeResque.Queue({ connection: connectionDetails }, jobs)
 
   queue.on('error', (error) => console.log(chalk.red(error)))
 
   await queue.connect(() => {
-    // queue.enqueue('math', 'add', [1, 2]);
-    // queue.enqueue(3000, 'math', 'add', [2, 1]);
     // Can also be called outside, or using await
     console.log("Queue Connected")
-    queue.enqueue('fetchDataQueue', "fetchData", "Jitendra Nirnejak", (err, data) => {
+    queue.enqueue('fetchRepositoriesQueue', "fetchRepositories", user, (err, data) => {
+      console.log(chalk.red(err))
+      console.log(chalk.blue(data))
+    })
+    queue.enqueue('fetchCollaboratorsQueue', "fetchCollaborators", user, (err, data) => {
+      console.log(chalk.red(err))
+      console.log(chalk.blue(data))
+    })
+    queue.enqueue('fetchCollaboratorDetailsQueue', "fetchCollaboratorDetails", user, (err, data) => {
       console.log(chalk.red(err))
       console.log(chalk.blue(data))
     })
 
-    const worker = new NR.Worker({ connection: connectionDetails, queues: ['emailQueue'] }, jobs)
+    const worker = new NodeResque.Worker({ connection: connectionDetails, queues: ['fetchRepositoriesQueue', 'fetchCollaboratorsQueue', 'fetchCollaboratorDetailsQueue'] }, jobs)
 
     worker.connect(() => {
       worker.workerCleanup()
@@ -53,5 +59,18 @@ async function processQueue() {
   })
   console.log(queue)
 }
+
+// Call the Worker if file is executed directly
+mongoose.connect(config.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log(chalk.green('🔥  MongoDB Connected...'))
+    User.find({}, async (err, users) => {
+      if (err) {
+        console.log(chalk.red("❗️  Users not found!"))
+      } else {
+        users.forEach(user => processQueue({ login: user.login }))
+      }
+    })
+  })
 
 module.exports = processQueue
