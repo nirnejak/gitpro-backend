dd0af1c579f99fa88f752ab603989d8ddcdb0e24
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-09-11 19:57:19 +0530

    Collaborators API Endpoint Updated for New Schema

--- routes/api/collaborators.js
+++ routes/api/collaborators.js
@@ -10,39 +10,27 @@ const router = express.Router();
 
 router.get('/', isAuthenticated, (req, res) => {
   // console.log(req.query.name)
-  User.findOne({ login: req.user.login }, (err, user) => {
-    if (err) {
-      res.status(404).json({ message: "User not Found" })
-    } else {
-      res.json(user.collaborators)
-    }
-  })
+  Collaborator.find({ owner: req.user.login })
+    .then(collaborators => {
+      res.json(collaborators)
+    })
+    .catch(err => {
+      console.log(chalk.red(err))
+      res.status(500).json({ message: "Something went wrong" })
+    })
 })
 
 router.get('/:login', isAuthenticated, (req, res) => {
-  // TODO: New Query Based on New Model
-  // Collaborator.findOne({ login: req.params.login })
-  //   .populate("repo")
-  //   .then(collaborator => {
-  //     res.json(collaborator)
-  //   })
-  //   .catch(err => {
-  //     console.log(chalk.red(err))
-  //     res.status(404).json({ message: "Collaborator not Found" })
-  //   })
-  User.findOne({ login: req.user.login }, (err, user) => {
-    if (err) {
-      res.status(404).json({ message: "User not Found" })
-    } else {
-      let collaborator = user.collaborators.filter(collaborator => collaborator.login === req.params.login)
-      if (collaborator.length >= 1) {
-        collaborator = collaborator[0]
-        res.json(collaborator)
-      } else {
-        res.status(404).json({ message: "Collaborator not found" })
-      }
-    }
-  })
+  Collaborator.findOne({ owner: req.user.login, login: req.params.login })
+    .populate("repo")
+    .then(collaborator => {
+      if (collaborator) res.json(collaborator)
+      else res.status(404).json({ message: "Collaborator not found" })
+    })
+    .catch(err => {
+      console.log(chalk.red(err))
+      res.status(500).json({ message: "Something went wrong" })
+    })
 })
 
 router.post('/', isAuthenticated, (req, res) => {
