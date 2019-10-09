5413bb3962ccb82cc82d532b0ab6212f623832f1
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-10-09 16:14:59 +0530

    REDIS_URL

--- tasks/index.js
+++ tasks/index.js
@@ -3,13 +3,7 @@ const Redis = require('ioredis')
 
 const config = require('../config')
 
-const redisConfig = {
-  host: config.REDIS_HOST,
-  port: config.REDIS_PORT,
-  user: config.REDIS_USER,
-  password: config.REDIS_PASSWORD,
-  database: 0
-}
+let redisConfig = config.REDIS_URL
 
 const client = new Redis(redisConfig)
 const subscriber = new Redis(redisConfig)
@@ -22,11 +16,11 @@ const queueConfig = {
     }
   }
 }
-const fetchRepositoriesQueue = new Queue('fetchRepositoriesQueue', config.REDIS_URL);
-const fetchCollaboratorsQueue = new Queue('fetchCollaboratorsQueue', config.REDIS_URL);
-const fetchCollaboratorDetailsQueue = new Queue('fetchCollaboratorDetailsQueue', config.REDIS_URL);
-const removeCollaboratorFromRepoQueue = new Queue('removeCollaboratorFromRepoQueue', config.REDIS_URL);
-const sendInvitationToCollaborateQueue = new Queue('sendInvitationToCollaborateQueue', config.REDIS_URL);
+const fetchRepositoriesQueue = new Queue('fetchRepositoriesQueue', queueConfig);
+const fetchCollaboratorsQueue = new Queue('fetchCollaboratorsQueue', queueConfig);
+const fetchCollaboratorDetailsQueue = new Queue('fetchCollaboratorDetailsQueue', queueConfig);
+const removeCollaboratorFromRepoQueue = new Queue('removeCollaboratorFromRepoQueue', queueConfig);
+const sendInvitationToCollaborateQueue = new Queue('sendInvitationToCollaborateQueue', queueConfig);
 module.exports = {
   fetchRepositoriesQueue,
   fetchCollaboratorsQueue,
