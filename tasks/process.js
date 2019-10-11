fc7254e7770baa6d7899e3006605637e08bd718a
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-10-11 15:19:09 +0530

    Log Message Updated

--- tasks/process.js
+++ tasks/process.js
@@ -208,11 +208,13 @@ fetchRepositoriesQueue.process(async (job, done) => {
 
   try {
     let res = await axios.get("https://api.github.com/user/repos?per_page=20&page=1", { headers })
+    let linkHeader = []
     if (res.headers.link) {
-      let linkHeader = res.headers.link.split(',')
+      linkHeader = res.headers.link.split(',')
     } else {
       console.log(res.status)
       console.log(res.data)
+      done()
     }
     let lastLink = linkHeader.filter(link => link.includes("last"))[0]
     let lastPage = lastLink.replace(' <https://api.github.com/user/repos?per_page=20&page=', '').replace('>; rel="last"', '')
