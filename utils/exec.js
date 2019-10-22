59b37e31bce47739cab59618568cdc560d4f3f6e
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-10-22 17:27:12 +0530

    maxBuffer Updates

--- utils/exec.js
+++ utils/exec.js
@@ -3,6 +3,8 @@ const exec = require('child_process').exec;
 // Wrapper of the exec function with Promise
 module.exports = executeSystemCommand = (command) => {
   return new Promise((resolve, reject) => {
+    // exec(command, { maxBuffer: Infinity }, (error, stdout, stderr) => {
+    // exec(command, { maxBuffer: 10 * 1024 * 1024 }, (error, stdout, stderr) => {
     exec(command, (error, stdout, stderr) => {
       if (error) {
         return reject(error)
